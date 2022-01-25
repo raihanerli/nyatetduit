dac6addd0ac9ccbf70c50f94122cac3a6d2601aa
Raihan Erlii <raihanerlianti@gmail.com>
2022-01-25 21:01:33 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" >
+		<title>Nyatet Duit by Raihanerli</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+				<link rel="icon" href="img/transbglogo.png" type="image/x-icon">
+		<!-- Google Fonts -->
+				<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:100,200,300,400,600,500,700,800,900|Nunito:100,200,300,400,500,600,700,800,900&amp;subset=latin" rel="stylesheet">
+				<!-- Bootstrap 4.3.1 CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<!-- Slick 1.8.1 jQuery plugin CSS (Sliders) -->
+		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+		<!-- Fancybox 3 jQuery plugin CSS (Open images and video in popup) -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+		<!-- AOS 2.3.1 jQuery plugin CSS (Animations) -->
+		<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+		<!-- FontAwesome CSS -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+		<!-- Startup 3 CSS (Styles for all blocks) -->
+					<link href="css/style.css" rel="stylesheet" />
+				<!-- jQuery 3.3.1 -->
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+			</head> 
+	<body>
+
+	
+<!-- Header 3 -->
+
+<header class="pt-0 pb-15
+0 bg-light header_3">
+	
+<!-- Ecommerce 25 -->
+
+<style>
+	.ecommerce_25{
+		background-image:url(img/bg.png);
+	}
+	@media(-webkit-min-device-pixel-ratio: 1.25), (min-resolution: 120dpi){
+		.ecommerce_25{
+			background-image:url(img/bg.png);
+		}
+	}
+</style>
+<div class="row justify-content-center">
+		<div class="small" data-aos-duration="600" data-aos="fade-down" data-aos-delay="400">
+		<section class="ecommerce_25 bg-dark pt-100 pb-100 text-center">
+			<img src="img/logoawal.png" class="img-fluid" alt="Responsive image">
+		</section>
+		</div>
+</div>
+</header>
+
+
+<!-- Carrousel -->
+<div class="container px-xl-0">
+	<div class="row justify-content-between align-items-baseline">
+		<div class="col-xl-8 col-lg-7 d-flex align-items-baseline" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+				<ol class="carousel-indicators">
+				  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				</ol>
+				<div class="carousel-inner">
+				  <div class="carousel-item active">
+					<img src="img/slide1.png" class="d-block h-600" alt="Responsive">
+				  </div>
+				  <div class="carousel-item">
+					<img src="img/slide2.png" class="d-block h-600" alt="Responsive">
+				  </div>
+				  <div class="carousel-item">
+					<img src="img/slide3.png" class="d-block h-600" alt="Responsive">
+				  </div>
+				</div>
+				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				  <span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				  <span class="sr-only">Next</span>
+				</a>
+			  </div>
+			</div>
+		</div>
+	</div>	
+
+
+	<!-- Header Menu 3 -->
+
+	<nav class="header_menu_3 pt-30 pb-30 mt-35">
+		<div class="container px-xl-0">
+			<div class="row justify-content-between align-items-baseline">
+				<div class="col-xl-8 col-lg-7 d-flex align-items-baseline" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
+					<div class="mr-15 logo">Nyatet Duit</div>
+					<div class="px-15 mr-5 dropdown">
+						<a href="index.html" class="link color-heading f-18 medium mx-15">Home</a>
+					</div>
+					<a href="pengeluaran.html" class="link color-heading f-18 medium mx-15">Pengeluaran</a>
+					<a href="pemasukan.html" class="link color-heading f-18 medium mx-15">Pemasukan</a>
+					<a href="akun.html" class="link color-heading f-18 medium mx-15">Account</a>
+				</div>
+				<div class="col-xl-4 col-lg-5 d-flex justify-content-end align-items-center" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
+					<a href="Sign Up.html" class="ml-15 btn sm action-1">Sign Up</a>
+				</div>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container px-xl-0">
+		<div class="row justify-content-center">
+			<div class="col-lg-12 col-md-8">
+				<div class="mb-3 mb-lg-5 logo d-block d-xl-none text-center text-lg-left logo_mobile">Nyatet Duit</div>
+			</div>
+		</div>
+		<div class="row justify-content-lg-between justify-content-center">
+			<div class="col-xl-7 col-lg-6 col-md-8 text-center text-lg-left">
+				<h1 class="small" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">New way to calculate your own money!</h1>
+				<form class="text-center mt-75" action="form-handler.php" method="POST" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
+					<div class="row">
+						<div class="col-xl-9">
+							<input type="uname" name="uname" class="mb-30 input w-full lg border-gray focus-heading color-heading placeholder-heading" placeholder="Username" required />
+							<input type="password" name="password" class="mb-30 input w-full lg border-gray focus-heading color-heading placeholder-heading" placeholder="Password" required />
+							<button class="btn w-full lg action-1">Login</button>
+
+							<div class="mt-15 color-heading text-adaptive">Don't have an account? <a class="link color-heading" href="Sign Up.html">Sign Up</a></div>
+						</div>
+					</div>
+				</form>
+			</div>
+			
+		</div>		
+	</div>
+
+<!-- Footer 5 -->
+
+<footer class="footer_5 bg-light pt-80 pb-40">
+	<div class="container px-xl-0">
+		<div class="row">
+			<div class="col-lg-3 col-md-6 order-0" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
+				<div class="row">
+					<div class="col-12">
+						<a href="#" class="mb-30 logo link color-main">Nyatet Duit</a>
+					</div>
+					<div class="col-xl-9 color-heading text-adaptive">
+						Be sure to take a look at 
+						our <a href="#" class="link color-heading">Terms of Use</a> and 
+						<a href="#" class="link color-heading">Privacy Policy</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-1 d-none d-xl-block"></div>
+			<div class="mt-40 mt-lg-0 col-lg-4 order-2 order-lg-0">
+				<div class="row">
+					<div class="col-6" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
+						<div class="mb-30 f-18 semibold title">About</div>
+						<div class="mb-10"><a href="#" class="link color-main">About Us</a></div>
+						<div class="mb-10"><a href="#" class="link color-main">Blog</a></div>
+						<div class="mb-10"><a href="#" class="link color-main">Team</a></div>
+						<div class="mb-10"><a href="#" class="link color-main">Career</a></div>
+						<div class="mb-10"><a href="#" class="link color-main">Contact</a></div>
+					</div>
+					<div class="col-6" data-aos-duration="600" data-aos="fade-down" data-aos-delay="600">
+						<div class="mb-30 f-18 semibold title">Company</div>
+						<div class="mb-10"><a href="#" class="link color-main">Privacy</a></div>
+						<div class="mb-10"><a href="#" class="link color-main">Support</a></div>
+						<div class="mb-10"><a href="#" class="link color-main">Help Desk</a></div>
+						<div class="mb-10"><a href="#" class="link color-main">FAQ</a></div>
+					</div>
+				</div>
+			</div>
+			<div class="mt-40 mt-md-0 col-lg-4 col-md-6 order-1 order-lg-0" data-aos-duration="600" data-aos="fade-down" data-aos-delay="900">
+				<div class="mb-30 f-18 semibold title">Subscribe our Newsletters</div>
+				<form method="POST" action="form-handler.php" class="row no-gutters">
+					<div class="col-md-7 col-sm-6 col-7">
+						<input type="email" name="email" placeholder="Your email" class="w-full input sm border-gray focus-action-1 color-heading placeholder-heading" required />
+					</div>
+					<div class="col-md-5 col-sm-4 col-5">
+						<div class="ml-10">
+							<button class="btn sm action-1 w-full f-16">Subscribe</button>
+						</div>
+					</div>
+				</form>
+				<div class="mt-45 socials">
+					<a href="#" class="f-18 link color-main mr-30"><i class="fab fa-twitter"></i></a>
+					<a href="#" class="f-18 link color-main mr-30"><i class="fab fa-facebook-square"></i></a>
+					<a href="#" class="f-18 link color-main mr-30"><i class="fab fa-google-plus-g"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="mt-110 mb-35 hr h-2 bg-gray op-5" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0"></div>
+		<div class="color-heading text-center text-adaptive" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
+			Startup Framework contains components and complex blocks 
+			which can easily be integrated into almost any design. 
+		</div>
+	</div>
+</footer>
+
+
+<!-- forms alerts -->
+<div class="alert alert-success alert-dismissible alert-form-success" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	Berhasil!
+</div>
+<div class="alert alert-warning alert-dismissible alert-form-check-fields" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	Tolong isi dulu ya!
+</div>
+<div class="alert alert-danger alert-dismissible alert-form-error" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	Maaf, error! Databasenya belum siap.
+</div>
+
+<!-- Bootstrap 4.3.1 JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+<!-- Fancybox 3 jQuery plugin JS (Open images and video in popup) -->
+<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+<!-- Google maps JS API -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDP6Ex5S03nvKZJZSvGXsEAi3X_tFkua4U"></script>
+<!-- Slick 1.8.1 jQuery plugin JS (Sliders) -->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<!-- AOS 2.3.1 jQuery plugin JS (Animations) -->
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<!-- Maskedinput jQuery plugin JS (Masks for input fields) -->
+<script src="js/jquery.maskedinput.min.js"></script>
+<!-- Startup 3 JS (Custom js for all blocks) -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
